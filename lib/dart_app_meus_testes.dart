44578f03e4e52528e5fc24e14d093b99233a196e
Elbis Silva <elbissilva@gmail.comm>
2022-12-23 22:49:35 -0300

    commit2

--- lib/dart_app_meus_testes.dart
+++ lib/dart_app_meus_testes.dart
@@ -1,4 +1,13 @@
 double caulcularDesconto (double valor, double desconto, bool porcentagem) {
+  if (valor <= 0){
+    throw ArgumentError("O produto está zerado");
+  }
+  // if (desconto <= 0){
+  //   print(valor);
+  // }
+  if (desconto<=0){
+    throw ArgumentError("Produto não tem desconto");
+  }
   if(porcentagem) {
     return valor - ( valor * desconto )/ 100;
   }
--- test/dart_app_meus_testes_test.dart
+++ test/dart_app_meus_testes_test.dart
@@ -5,8 +5,17 @@ void main() {
   test('Caucular desconto com porcentagem', () {
     expect(teste.caulcularDesconto(10000, 20, true), 8000);
   });
-
-    test('Caucular desconto desconto sem porcentagem', () {
+   test('Caucular desconto sem porcentagem com preço igual a zero', () {
+    expect(()=> teste.caulcularDesconto(0, 20, false),
+    throwsA(TypeMatcher<ArgumentError>()));
+  });
+  //  test('Caucular desconto com desconto zerado', () {
+  //   expect(teste.caulcularDesconto(10000, 0, true), 10000);
+  // });
+   test('Caucular desconto com esconto zerado recebendo type', () {
+    expect(()=>teste.caulcularDesconto(10000, 0, true), throwsA(TypeMatcher<ArgumentError>()));
+  });
+      test('Caucular desconto desconto sem porcentagem', () {
     expect(teste.caulcularDesconto(10000, 9500, false), 500);
   });
 }
